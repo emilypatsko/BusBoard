c15cf1da5a204f92fcd77a4947a82432b4bbaf72
Emily Patsko <Emily.Patsko@softwire.com>
2021-01-22 15:06:30 +0000

    Functionality added: accepts user-input bus stop code; sorts departures by soonest expected departure time; only prints out soonest 5.

--- busboard.js
+++ busboard.js
@@ -1,21 +1,58 @@
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+function printDepartureInfo(departure) {
+    let arr = [departure.line, departure.aimed_departure_time, "Expected:", departure.expected_departure_time, departure.direction];
+    console.log(arr.join(' '));
+}
+
+function extractDepartureInfo(departure) {
+
+}
+
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const prompt = require('prompt-sync')();
+
+// Get bus stop code
+const busStop = prompt('Please enter a bus stop code: ');
 
 var request = new XMLHttpRequest();
+var url = 'http://transportapi.com/v3/uk/bus/stop/' + busStop + '/live.json?group=route&app_id=97d91d05&app_key=b77e693ec08272f32658588da099e89f';
 
-request.open('GET', 'http://transportapi.com/v3/uk/bus/stop/490008660N/live.json?group=route&app_id=97d91d05&app_key=b77e693ec08272f32658588da099e89f', true)
-
+request.open('GET', url, true)
 var response;
 
 request.onload = function () {
-    // do something with the retrieved data, stored in request.response
+    // store all the requested data
     response = JSON.parse(request.responseText);
+
+    // Extract the information on bus departures
     departures = response.departures;
+  
+    var departureBoard = [];
 
     for (let busRoute in departures) {
-        departures[busRoute].forEach(departure => 
-            console.log([departure.line, departure.direction, departure.expected_departure_time].join(' '))
-            );
+        departureBoard = departureBoard.concat(Object.values(departures[busRoute]));
     }       
+
+    departureBoard.sort(function(a, b) {
+        if (a.expected_departure_time < b.expected_departure_time) {
+            return -1;
+        } else if (a.expected_departure_time > b.expected_departure_time) {
+            return 1;
+        } else { 
+            if (a.aimed_departure_time < b.aimed_departure_time) {
+                return -1;
+            } else if (a.aimed_departure_time > b.aimed_departure_time) {
+                return 1;
+            } else {
+                return 0;  
+            }          
+        }
+    })
+
+    let i = 0;
+    while (i < departureBoard.length && i < 5) {
+        printDepartureInfo(departureBoard[i]);
+        i++;
+    }
 }
 
 request.send();
